dd3fa26d457c74df0ef553591a13ffce3bb6686a
JakeSiewJK64 <joekanesiew@outlook.com>
2025-05-30 16:08:57 +0800

    remove unused urls

--- pharmacytest/urls.py
+++ pharmacytest/urls.py
@@ -22,7 +22,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/home', views.home),
     path('api/v1/medicines/get_medicines', views.get_medicines),
     path('api/v1/medicines/export_medicines_csv', views.export_medicines_csv),
     path('api/v1/medicines/create_medicine', views.create_medicines),
@@ -30,5 +29,4 @@ urlpatterns = [
     # OpenAPI schema generation
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
